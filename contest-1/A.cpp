99505e64a97828654b8b0b65ed8bdfee8ddee76d
Ganat Ahmed <ganata428@gmail.com>
2026-07-09 05:32:49 +0300

    Delete contest-1 directory

--- contest-1/A.cpp
+++
@@ -1,8 +0,0 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     double x;
-//     double p;
-//     cin>>x>>p;
-//     cout<<(p*100)/((100-x));
-// }
--- contest-1/C.cpp
+++
@@ -1,8 +0,0 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     char ch;
-//     cin>>ch;
-//     if(ch=='z')cout<<'a'<<endl;
-//     else   cout<<(char)(1+ch)<<endl;
-// }
