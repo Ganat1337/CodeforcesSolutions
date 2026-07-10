0bf1958b0a1d9bd0fb732afc52c94e07b9b4755c
Ganat Ahmed <ganata428@gmail.com>
2026-07-10 07:35:35 +0300

    Add files via upload

---
+++ sheet2/E.cpp
@@ -0,0 +1,13 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int num,max=0,nums;
+//     cin>>num;
+//     for(int i=0;i<num;i++){
+//         cin>>nums;
+//         if(max<nums){
+//             max=nums;  
+//         }
+//     }
+//     cout<<max;
+// }
