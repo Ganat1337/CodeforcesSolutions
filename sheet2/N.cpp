308ddfa4df6a24a7389707f70f25e59cc7705ff4
Ganat Ahmed <ganata428@gmail.com>
2026-07-14 03:56:14 +0300

    Update N.cpp

--- sheet2/N.cpp
+++ sheet2/N.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    char s;
+    int num,nums;
+    cin>>s>>num;
+    for(int i=1; i<=num;i++){
+        cin>>nums;
+        for(int j=1; j<=nums; j++){
+            cout<<s;
+        }
+    cout<<'\n';    
+    }
 }
